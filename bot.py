6e67f7e2410edb84da5cfe996f17b48ca266d545
Men4ikLiga <planetaneptun17@gmail.com>
2025-11-01 22:18:42 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import os
 # Настройка логирования
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 # Расписание уроков
 SCHEDULE = {
